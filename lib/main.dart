6136155e7467684fd26b0db615de51f58fccac32
Lourrayni <lourraynidantass@gmail.com>
2025-11-16 00:52:45 -0300

    feat(main): adding  log, FlutterError to catch framework errors, and PlatformDispatcher to catch untreated platform errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:random_teams/viewmodel/jogador_viewmodel.dart';
 import 'view/home_view.dart';
+import 'dart:developer';
 
 void main() {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+    log("FlutterError", error: details.exception, stackTrace: details.stack);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    log("Erro de plataforma", error: error, stackTrace: stack);
+    return true;
+  };
+  log("iniciando app");
   runApp(ChangeNotifierProvider(
     create: (_) => JogadorViewModel(),
     child: const SorteadorApp()
